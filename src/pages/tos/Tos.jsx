35373724f9f76b7d64b8a02668b9c6a331fc2676
donatto22 <ottanod22@gmail.com>
2023-02-22 22:00:09 -0500

    Empezando términos y servicios

--- src/pages/tos/Tos.jsx
+++ src/pages/tos/Tos.jsx
@@ -1,5 +1,47 @@
-const Tos = () => {
+import './tos.css'
 
+const Tos = () => {
+    return (
+        <div id="container-tos">
+            <div id="tos">
+                <div id="title-tos">
+                    <div>Términos y Condiciones</div>
+                </div>
+
+                <div id="description-tos">
+                    <div className="termino">
+                        <div className="encabezado">
+                            Privacidad
+                        </div>
+
+                        <div className="descripcion">
+
+                        </div>
+                    </div>
+
+                    <div className="termino">
+                        <div className="encabezado">
+
+                        </div>
+
+                        <div className="descripcion">
+                            
+                        </div>
+                    </div>
+
+                    <div className="termino">
+                        <div className="encabezado">
+
+                        </div>
+
+                        <div className="descripcion">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Tos
